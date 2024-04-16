a79fe2a7e0e607ad6ec6815196cc176606b32476
gengningsheng <359315634@qq.com>
2024-04-16 17:00:27 +0800

    feat: ✨ 接入elementPlus+unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,57 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    // head
+    head: {
+      title: "Element Plus + Nuxt 3",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "ElementPlus + Nuxt3",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+  // css
+  css: ["~/assets/scss/index.scss"],
+  typescript: {
+    strict: true,
+    shim: false,
+  },
+  // build modules
+  modules: [
+    "@unocss/nuxt",
+    "@pinia/nuxt",
+    "@element-plus/nuxt",
+    "@nuxtjs/color-mode",
+  ],
+  // colorMode
+  colorMode: {
+    classSuffix: "",
+  },
+  unocss: {
+    uno: true,
+    attributify: true,
+    icons: {
+      scale: 1.2,
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/scss/element/index.scss" as element;`,
+        },
+      },
+    },
+  },
+  elementPlus: {
+    icon: "ElIcon",
+    importStyle: "scss",
+    themes: ["dark"],
+  },
+  devtools: { enabled: true },
+});
